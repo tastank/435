67a90b5e71ba2f1c0035a189c84bbcb6af87bc82
Tyler A Stank <tbear2500@gmail.com>
2015-11-02 20:03:51 -0600

    Initial commit

---
+++ project/xor.cpp
@@ -0,0 +1,13 @@
+#include <cstdlib>
+#include <cstdio>
+
+
+int main(int argc, char *argv[]) {
+    //xor filename
+    if (argc != 2) {
+        printf("Wrong number of args.\n");
+        return -1;
+    }
+
+    return 0;
+}
